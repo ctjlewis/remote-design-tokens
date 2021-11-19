948cdc4f33409114c8e982103ea81bfbc7adb931
C. Lewis <ctj.lewis@icloud.com>
2021-11-19 13:28:11 -0600

    chore: update example

--- example/index.tsx
+++ example/index.tsx
@@ -2,12 +2,25 @@ import 'react-app-polyfill/ie11';
 
 // @ts-ignore
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+import { fromAirtable } from '../src/providers/airtable';
+
+const apiKey = process.env.AIRTABLE_KEY ?? "";
+const base = process.env.AIRTABLE_BASE ?? "";
+
+const tokens = await fromAirtable({
+  apiKey,
+  base,
+  table: "Icons",
+  tokenField: "iconXIDd",
+  propFields: ["icon", "vector"],
+});
 
 const App = () => {
   return (
     <div>
-      <Thing />
+      {
+        JSON.stringify(tokens, null, 2)
+      }
     </div>
   );
 };
